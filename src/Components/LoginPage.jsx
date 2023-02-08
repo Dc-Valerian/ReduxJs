6e26c7067302fa933e4ed9014b0500791ea7aa0d
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-08 13:22:13 +0100

    added changes

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { useDispatch } from 'react-redux'
+import { LoginUser } from './ReduxState/ReduceState'
 
 const LoginPage = () => {
+  const dispatch = useDispatch()
   return (
     <div>
     <div>
-    <button>
+    <button
+    onClick={()=>{
+      dispatch(LoginUser({UserName:"Valerian",Age:20,Email:"valerianpedro03@gmail",Stack:"fullstack"}))
+    }}
+    >
     Login
     </button>
     <button>

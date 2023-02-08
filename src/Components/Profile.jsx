81172e6694a2d6a5a9ee20fc9e44c392163a5692
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-08 13:43:43 +0100

    added changes to profile

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,21 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+  const user = useSelector((state)=>state.myReducer.current)
   return (
     <div>
-    <h2>User</h2>
-    <div>
-    UserName:
-    </div>
-    <div>
-Age:
-    </div>
-    <div>
-    Email:
-    </div>
-    <div>
-    Stack:
-    </div>
+    <div>UserName:{user?.userName}</div>
+    <div>Age:{user?.Age}</div>
+    <div>Email:{user?.Email}</div>
+    <div>Stack:{user?.Stack}</div>
     </div>
   )
 }
